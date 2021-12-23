f49590abc338d4203e79d6f0e54c6c5025719d83
Acer <Acer@HELL1SHFIRE>
2021-12-23 14:04:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// For Directory.GetFiles and Directory.GetDirectories
+// For File.Exists, Directory.Exists
+using System;
+using System.IO;
+using System.Collections;
+using System.Linq;
+
+public class RecursiveFileProcessor
+{
+    public static void Main()
+    {
+        string path = "D:\\Modeling SSD\\temp2";
+
+        if (File.Exists(path))
+        {
+            // This path is a file
+            ProcessFile(path);
+        }
+        else if (Directory.Exists(path))
+        {
+            // This path is a directory
+            ProcessDirectory(path);
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a valid file or directory.", path);
+        }
+
+    }
+
+    // Process all files in the directory passed in, recurse on any directories
+    // that are found, and process the files they contain.
+    public static void ProcessDirectory(string targetDirectory)
+    {
+        // Process the list of files found in the directory.
+        string[] fileEntries = Directory.GetFiles(targetDirectory);
+        foreach (string fileName in fileEntries)
+        {
+            ProcessFile(fileName);
+        }
+        // Recurse into subdirectories of this directory.
+        string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+        foreach (string subdirectory in subdirectoryEntries)
+            ProcessDirectory(subdirectory);
+    }
+
+    // Insert logic for processing found files here.
+    public static void ProcessFile(string path)
+    {
+        //Console.WriteLine("ProcessFile called ");
+        string[] lodtemplate = new string[5];
+        string[] lodprefix = new string[5];
+        for (int i = 0; i < 5; i++)
+        {
+            //Console.WriteLine("lodtemplate entered ");
+            lodtemplate[i] = "_" + Convert.ToString(i) + ".obj";
+            if (i * 32 < 100)
+            {
+                lodprefix[i] = i + "_00" + (i * 32).ToString() + "_";
+            }
+            else
+            {
+                lodprefix[i] = i + "_0" + (i * 32).ToString() + "_";
+            }
+        }
+
+        // main thing here
+        for (int i = 1; i < 5; i++) // moving only from 1 to 4
+        {
+            if (path.EndsWith(lodtemplate[i])) // checks for _i.obj
+            {
+                string fileName = Path.GetFileName(path); // cuts path
+                string[] cut = fileName.Split(lodtemplate[i]); // cuts _i.obj
+                //Console.WriteLine(Path.GetFullPath(path));
+                //Console.WriteLine(" \n" + lodprefix[i] + cut[0] + " \n");
+                if (File.Exists(path))
+                {
+                    File.Move(path, Path.GetDirectoryName(path) + "\\" + lodprefix[i] + cut[0]); // actual renaming
+                }
+                else
+                {
+                    Console.WriteLine("File not found. \n");
+                }
+            }
+        }            
+    }
+}
